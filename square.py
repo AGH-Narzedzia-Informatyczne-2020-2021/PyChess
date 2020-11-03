647704a7c1476b0057b98cef91eddcf2788134a4
Your Name <you@example.com>
2020-11-03 09:58:49 +0100

    TEST

--- square.py
+++ square.py
@@ -5,6 +5,5 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit(0)
-
     pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(0, 0, 100, 100))
     pygame.display.flip()
