87f0b34d8b9ffffafdb4d4485bd8ca7d3d7a478b
Anna Ostrowska <ana.os@wp.pl>
2020-11-14 15:41:21 +0100

    knight commit

---
+++ chess/figures/Knight.py
@@ -0,0 +1,67 @@
+from chess.constants import WHITE, BLACK, SQUARE_SIZE
+import pygame as pg
+
+class Knight:
+    def __init__(self, color):
+        self.color = color
+        if self.color == WHITE:
+            self.image = pg.transform.scale(pg.image.load('figures_graphics/wN.png'), (SQUARE_SIZE, SQUARE_SIZE))
+        else:
+            self.image = pg.transform.scale(pg.image.load('figures_graphics/bN.png'), (SQUARE_SIZE, SQUARE_SIZE))
+
+
+    #chodzenie
+    def physically_possible_moves(self, row, column):
+
+        moves = []
+
+        if self.color == WHITE:
+            moves.append((row+2), (column+1)
+            moves.append((row+2), (column-1))
+            moves.append((row+1), (column+2))
+            moves.append((row+1), (column-2))
+            moves.append((row-2), (column+1)
+            moves.append((row-2), (column-1))
+            moves.append((row-1), (column+2))
+            moves.append((row-1), (column-2))
+
+        if self.color == BLACK:
+            moves.append((row + 2), (column + 1)
+            moves.append((row + 2), (column - 1))
+            moves.append((row + 1), (column + 2))
+            moves.append((row + 1), (column - 2))
+            moves.append((row - 2), (column + 1)
+            moves.append((row - 2), (column - 1))
+            moves.append((row - 1), (column + 2))
+            moves.append((row - 1), (column - 2))
+
+    #bicie
+    def physically_possible_captures(self, row, column):
+
+        moves = []
+
+        if self.color == WHITE:
+            moves.append((row + 2), (column + 1)
+            moves.append((row + 2), (column - 1))
+            moves.append((row + 1), (column + 2))
+            moves.append((row + 1), (column - 2))
+            moves.append((row - 2), (column + 1)
+            moves.append((row - 2), (column - 1))
+            moves.append((row - 1), (column + 2))
+            moves.append((row - 1), (column - 2))
+
+        if self.color == BLACK:
+            moves.append((row + 2), (column + 1)
+            moves.append((row + 2), (column - 1))
+            moves.append((row + 1), (column + 2))
+            moves.append((row + 1), (column - 2))
+            moves.append((row - 2), (column + 1)
+            moves.append((row - 2), (column - 1))
+            moves.append((row - 1), (column + 2))
+            moves.append((row - 1), (column - 2))
+
+    def is_ally(self, another_figure):
+        if self.color == another_figure.color:
+            return True
+        else:
+            return False
