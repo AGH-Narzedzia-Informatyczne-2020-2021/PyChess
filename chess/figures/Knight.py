9d459095fe1ea387392945613aba341075db4b08
ostrowskaanna <72552734+ostrowskaanna@users.noreply.github.com>
2020-11-14 15:48:34 +0100

    Update Knight.py

--- chess/figures/Knight.py
+++ chess/figures/Knight.py
@@ -16,14 +16,14 @@ class Knight:
         moves = []
 
         if self.color == WHITE:
-            moves.append((row+2), (column+1))
-            moves.append((row+2), (column-1))
-            moves.append((row+1), (column+2))
-            moves.append((row+1), (column-2))
-            moves.append((row-2), (column+1))
-            moves.append((row-2), (column-1))
-            moves.append((row-1), (column+2))
-            moves.append((row-1), (column-2))
+            moves.append((row + 2), (column + 1))
+            moves.append((row + 2), (column - 1))
+            moves.append((row + 1), (column + 2))
+            moves.append((row + 1), (column - 2))
+            moves.append((row - 2), (column + 1))
+            moves.append((row - 2), (column - 1))
+            moves.append((row - 1), (column + 2))
+            moves.append((row - 1), (column - 2))
 
         if self.color == BLACK:
             moves.append((row + 2), (column + 1))
