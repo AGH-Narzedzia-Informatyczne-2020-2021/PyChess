c5af4360357f6f1ce165adba58a8dbc5ff5a0acb
Paweł Wojciechowski <jagodek@student.agh.edu.pl>
2020-11-17 15:30:58 +0100

    Make Figure.py abstact. Init example.py.

--- chess/figures/Figure.py
+++ chess/figures/Figure.py
@@ -1,16 +1,36 @@
-class Figure:
-    def __init__(self, column, row, color):
+from abc import ABC, abstractmethod
+
+
+class Figure(ABC):
+    def __init__(self, column, row, color, pieces):
         self.column = column
         self.row = row
         self.color = color
+        self.pieces = pieces
 
     def move(self, column, row):
-        print("moved")
-        self.column = column
-        self.row = row
+        if (column, row) in self.get_possible_moves():
+            self.column = column
+            self.row = row
+            return True
+
+        if (column, row) in self.get_possible_captures():
+            for figure in self.figures:
+                if (figure.column, figure.row) == (column, row):
+                    self.figures.remove(figure)
+                    return True
+        return False
 
     def is_ally(self, another_figure):
         if self.color == another_figure.color:
             return True
         else:
             return False
+
+    @abstractmethod
+    def get_possible_moves(self):
+        pass
+
+    @abstractmethod
+    def get_possible_captures(self):
+        pass
