659b6123dbdcde1ede321cc5b1f20f94ba72937e
PiotrSzczachor <piotr.szczachor1@wp.pl>
2020-11-22 15:53:27 +0100

    Pawn Update

--- chess/pieces/Pawn.py
+++ chess/pieces/Pawn.py
@@ -28,13 +28,13 @@ class Pawn(Piece):
                 elif self.pieces.get((self.column, self.row + 2)) is not None and self.pieces.get((self.column, self.row + 1)) is None:
                     possible_moves.append(Move(self, self.column, self.row + 1))
 
-            else:
+            elif self.row != 8:
                 if self.pieces.get((self.column, self.row + 1)) is None:
                     possible_moves.append(Move(self, self.column, self.row + 1))
-            if self.pieces.get((self.column + 1, self.row + 1)) is not None:
+            if self.column != 8 and self.row != 8 and self.pieces.get((self.column + 1, self.row + 1)) is not None:
                 if self.pieces.get((self.column + 1, self.row + 1)).is_white != self.is_white:
                     possible_moves.append(Move(self, self.column + 1, self.row + 1, self.pieces.get((self.column + 1, self.row + 1))))
-            if self.pieces.get((self.column - 1, self.row + 1)) is not None:
+            if self.column != 1 and self.row != 8 and self.pieces.get((self.column - 1, self.row + 1)) is not None:
                 if self.pieces.get((self.column - 1, self.row + 1)).is_white != self.is_white:
                     possible_moves.append(Move(self, self.column - 1, self.row + 1, self.pieces.get((self.column - 1, self.row + 1))))
         else:
@@ -44,13 +44,13 @@ class Pawn(Piece):
                     possible_moves.append(Move(self, self.column, self.row - 2))
                 elif self.pieces.get((self.column, self.row - 2)) is not None and self.pieces.get((self.column, self.row - 1)) is None:
                     possible_moves.append(Move(self, self.column, self.row - 1))
-            else:
+            elif self.row != 1:
                 if self.pieces.get((self.column, self.row - 1)) is None:
                     possible_moves.append(Move(self, self.column, self.row - 1))
-            if self.pieces.get((self.column - 1, self.row - 1)) is not None:
+            if self.column != 1 and self.row != 1 and self.pieces.get((self.column - 1, self.row - 1)) is not None:
                 if self.pieces.get((self.column - 1, self.row - 1)).is_white != self.is_white:
                     possible_moves.append(Move(self, self.column - 1, self.row - 1, self.pieces.get((self.column - 1, self.row - 1))))
-            if self.pieces.get((self.column + 1, self.row - 1)) is not None:
+            if self.column != 8 and self.row != 1 and self.pieces.get((self.column + 1, self.row - 1)) is not None:
                 if self.pieces.get((self.column + 1, self.row - 1)).is_white != self.is_white:
                     possible_moves.append(Move(self, self.column + 1, self.row - 1, self.pieces.get((self.column + 1, self.row - 1))))
 
